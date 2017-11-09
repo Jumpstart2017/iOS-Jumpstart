68b1e1dcfe04f152ab82df1759e12d135eeedc31
Gabo <gleyvamerino@smu.edu>
2017-11-09 16:30:06 -0600

    Colors added

---
+++ Jumpstart/Extensions/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  Jumpstart
+//
+//  Created by Gabriel I Leyva Merino on 11/9/17.
+//  Copyright © 2017 Jumpstart. All rights reserved.
+//
+import UIKit
+
+extension UIColor {
+    
+    open static let jRed = UIColor(red: 200/255, green: 70/255, blue: 48/255, alpha: 1)
+    open static let jBlue = UIColor(red: 63/255, green: 167/255, blue: 214/255, alpha: 1)
+    open static let jYellow = UIColor(red: 255/255, green: 210/255, blue: 63/255, alpha: 1)
+    open static let jGreen = UIColor(red: 14/255, green: 172/255, blue: 105/255, alpha: 1)
+    
+}
