ea5045c77f41dd006361c5436d0af5a3c057cf68
Sean Burrows <seanburrows@Seans-MacBook-Pro-2.local>
2018-02-08 15:28:56 -0600

    preparing for merge

---
+++ Jumpstart/ViewControllers/NewTaskViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewTaskViewController.swift
+//  Jumpstart
+//
+//  Created by Sean Burrows on 2/6/18.
+//  Copyright © 2018 Jumpstart. All rights reserved.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+    
+}
