a832717b86de210b0fc0f3244ef5fc3d0e0d8a69
EttaMari <emgodwin@smu.edu>
2018-05-04 10:00:06 -0500

    ProjectTask up

---
+++ Jumpstart/ViewModels/TaskViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  TaskViewModel.swift
+//  Jumpstart
+//
+//  Created by Etta Godwin on 5/3/18.
+//  Copyright © 2018 Jumpstart. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import ObjectMapper
+
+class TaskViewModel: NSObject {
+    
+    var subProject: SubProject!
+    var task: Task!
+    var user: UserModel!
+    
+    func getTasks(completionHandler: @escaping ([String:Any]?, Error?) -> ()) {
+        getTasksCall(completionHandler: completionHandler)
+    }
+    
+    private func getTasksCall(completionHandler: @escaping ([String:Any]?, Error?) -> ()) {
+        let JSONString  = Mapper().toJSONString(subProject, prettyPrint: true)
+        let parameters = convertToJSON(text: JSONString!)
+        print (parameters)
+        Alamofire.request("https://us-central1-jumpstart-f48ac.cloudfunctions.net/getsubprojectbyid2", method: .post, parameters: parameters).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                completionHandler(value as? [String:Any], nil)
+            case .failure(let error):
+                completionHandler(nil, error)
+            }
+            
+        }
+    }
+    
+    
+    func createTask(completionHandler: @escaping ([String:Any]?, Error?) -> ()) {
+        createTaskCall(completionHandler: completionHandler)
+    }
+    
+    private func createTaskCall(completionHandler: @escaping ([String:Any]?, Error?) -> ()) {
+        let JSONString  = Mapper().toJSONString(task, prettyPrint: true)
+        let parameters = convertToJSON(text: JSONString!)
+        Alamofire.request("https://us-central1-jumpstart-f48ac.cloudfunctions.net/createTask", method: .post, parameters: parameters, headers: nil).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                print(value)
+                completionHandler(value as? [String:Any], nil)
+            case .failure(let error):
+                completionHandler(nil, error)
+            }
+        }
+    }
+    
+}
+
