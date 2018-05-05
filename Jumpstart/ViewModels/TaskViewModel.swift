abbf3b1804003b94ab86fd44282c3671867f8e0c
Sean Burrows <sburrows@smu.edu>
2018-05-04 23:15:59 -0500

    Hooked up to backend

--- Jumpstart/ViewModels/TaskViewModel.swift
+++ Jumpstart/ViewModels/TaskViewModel.swift
@@ -23,7 +23,7 @@ class TaskViewModel: NSObject {
     private func getTasksCall(completionHandler: @escaping ([String:Any]?, Error?) -> ()) {
         let JSONString  = Mapper().toJSONString(subProject, prettyPrint: true)
         let parameters = convertToJSON(text: JSONString!)
-        print (parameters)
+//        print (parameters)
         Alamofire.request("https://us-central1-jumpstart-f48ac.cloudfunctions.net/getsubprojectbyid2", method: .post, parameters: parameters).responseJSON { response in
             switch response.result {
             case .success(let value):
@@ -43,6 +43,7 @@ class TaskViewModel: NSObject {
     private func createTaskCall(completionHandler: @escaping ([String:Any]?, Error?) -> ()) {
         let JSONString  = Mapper().toJSONString(task, prettyPrint: true)
         let parameters = convertToJSON(text: JSONString!)
+
       Alamofire.request("https://us-central1-jumpstart-f48ac.cloudfunctions.net/createTask", method: .post, parameters: parameters, headers: nil).responseJSON { response in
             switch response.result {
             case .success(let value):
@@ -54,10 +55,22 @@ class TaskViewModel: NSObject {
         }
     }
     
-    func getAllTasks(completionHandler: @escaping ([String:Any]?, Error?) -> ()) {}
+    func getAllTasks(completionHandler: @escaping ([String:Any]?, Error?) -> ()) {
+        getAllTasksCall(completionHandler: completionHandler)
+    }
     
     private func getAllTasksCall(completionHandler: @escaping ([String:Any]?, Error?) -> ()) {
-        
+        let JSONString  = Mapper().toJSONString(user, prettyPrint: true)
+        let parameters = convertToJSON(text: JSONString!)
+ 
+        Alamofire.request("https://us-central1-jumpstart-f48ac.cloudfunctions.net/getglobaltasks", method: .post, parameters: parameters, headers: nil).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                completionHandler(value as? [String:Any], nil)
+            case .failure(let error):
+                completionHandler(nil, error)
+            }
+        }
     }
 }
 
